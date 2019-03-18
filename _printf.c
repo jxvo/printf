233e17f25484962bcfab56cf8e9baeaf1bb4a4e4
Jonathan Vasquez <jxvo92@yahoo.com>
2019-03-18 22:39:32 +0000

    Initial commit

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: string to search through for conversion specifiers, and print
+ *
+ * Return: integer number of characters printed, excluding NULL byte
+ */
+
+int _printf(const char *format)
+{
+}
